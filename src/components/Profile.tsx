21356c67a2f3560c76bc3425864c100923f6805a
Jerson Zúñiga <jzunigacoayla@gmail.com>
2024-11-19 11:28:09 -0500

    feat: add Avatar component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,15 @@
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="https://i.imgur.com/1bX5QH6.jpg"
+      alt="Lin Lanying"
+      width={100}
+      height={100}
+    />
+  );
+}
+
 export default function Profile() {
-  return <img src="https://i.imgur.com/QIrZWGIs.jpg" alt="Alan L. Hart" />;
+  return <Avatar />;
 }

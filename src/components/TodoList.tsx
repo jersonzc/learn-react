3ce1be9a4707368b5d0604d90119bcd1a4c85c9a
Jerson Zúñiga <jzunigacoayla@gmail.com>
2024-11-19 10:39:27 -0500

    feat: avoid complex expressions

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,7 +2,7 @@ const baseUrl = "https://i.imgur.com/";
 const person = {
   name: "Gregorio Y. Zara",
   imageId: "7vQD0fP",
-  imageSize: "b",
+  imageSize: "s",
   theme: {
     backgroundColor: "black",
     color: "pink",
@@ -10,14 +10,12 @@ const person = {
 };
 
 export default function TodoList() {
+  const url = baseUrl + person.imageId + person.imageSize + ".jpg";
+
   return (
     <div style={person.theme}>
       <h1>{person.name}'s Todos</h1>
-      <img
-        className="avatar"
-        src={`${baseUrl}${person.imageId}${person.imageSize}.jpg`}
-        alt={person.name}
-      />
+      <img className="avatar" src={url} alt={person.name} />
       <ul>
         <li>Improve the videophone</li>
         <li>Prepare aeronautics lectures</li>

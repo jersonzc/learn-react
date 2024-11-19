9be536aaa566a48045f3d3ad88956ceab7aab0af
Jerson Zúñiga <jzunigacoayla@gmail.com>
2024-11-19 11:10:28 -0500

    feat: create image url from person data
    add Person object

---
+++ src/Person.ts
@@ -0,0 +1,8 @@
+import React from "react";
+
+export interface Person {
+  name: string;
+  imageId: string;
+  imageSize: string;
+  theme: React.CSSProperties;
+}
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,10 @@
-const baseUrl = "https://i.imgur.com/";
-const person = {
+import { Person } from "../Person.ts";
+import { getImageUrl } from "../utils.ts";
+
+const person: Person = {
   name: "Gregorio Y. Zara",
   imageId: "7vQD0fP",
-  imageSize: "s",
+  imageSize: "b",
   theme: {
     backgroundColor: "black",
     color: "pink",
@@ -10,12 +12,10 @@ const person = {
 };
 
 export default function TodoList() {
-  const url = baseUrl + person.imageId + person.imageSize + ".jpg";
-
   return (
     <div style={person.theme}>
       <h1>{person.name}'s Todos</h1>
-      <img className="avatar" src={url} alt={person.name} />
+      <img className="avatar" src={getImageUrl(person)} alt={person.name} />
       <ul>
         <li>Improve the videophone</li>
         <li>Prepare aeronautics lectures</li>

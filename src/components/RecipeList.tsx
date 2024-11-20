98b6400c9b21fb7d61259bbd2ae4f74b947ede80
Jerson Zúñiga <jzunigacoayla@gmail.com>
2024-11-20 11:45:31 -0500

    refactor: add Recipe component

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,19 +1,36 @@
 import { recipes } from "../data.js";
 import { Fragment } from "react";
 
+interface RecipeProps {
+  id: string;
+  name: string;
+  ingredients: string[];
+}
+
+function Recipe({ id, name, ingredients }: RecipeProps) {
+  return (
+    <Fragment key={id}>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map((recipe) => (
-        <Fragment key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map((ingredient) => (
-              <li key={ingredient}>{ingredient}</li>
-            ))}
-          </ul>
-        </Fragment>
+        <Recipe
+          key={recipe.id}
+          id={recipe.id}
+          name={recipe.name}
+          ingredients={recipe.ingredients}
+        />
       ))}
     </div>
   );

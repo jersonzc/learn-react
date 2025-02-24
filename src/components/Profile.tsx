5b5bfc1a2af2d6adbbfd7e5bf2ace05f4ddbf4a0
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-02-24 15:36:55 -0500

    feat: remove cards

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,5 @@
 import { getImageUrl } from "../utils.js";
 import { Person } from "../Person.ts";
-import Card from "./Card.tsx";
 
 const SIZE_LIMIT = 90;
 
@@ -45,25 +44,6 @@ export default function Profile({ person }: ProfileProps) {
           {person.discover}
         </li>
       </ul>
-      <div>
-        <Card>
-          <h2>Photo</h2>
-          <img
-            className="avatar"
-            src="https://i.imgur.com/OKS67lhm.jpg"
-            alt="Aklilu Lemma"
-            width={70}
-            height={70}
-          />
-        </Card>
-        <Card>
-          <h2>About</h2>
-          <p>
-            Aklilu Lemma was a distinguished Ethiopian scientist who discovered
-            a natural treatment to schistosomiasis.
-          </p>
-        </Card>
-      </div>
     </section>
   );
 }
